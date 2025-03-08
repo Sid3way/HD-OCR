17172482e70fc473bc9d7a0e35bf66a2ae3e6bdc
Sid3way <damien_simeon@hotmail.com>
2025-03-08 11:11:59 +0100

    Create an API structure and services to handle CV

---
+++ IHCWargames.Api/Controllers/ScoreController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using IHCWargames.Api.Models;
+
+namespace IHCWargames.Api.Controllers;
+
+public class ScoreController : ControllerBase
+{
+    [HttpPut]
+    public IActionResult Score(AddScoreCommand command)
+    {
+        return Ok();
+    }
+}
---
+++ IHCWargames.Api/Models/AddScoreCommand.cs
@@ -0,0 +1,31 @@
+﻿namespace IHCWargames.Api.Models;
+
+public enum FleetEnum
+{
+    // First Armada
+    Minutemen,
+    Pathfinders,
+    Roughnecks,
+    
+    // Second Armada
+    Beowulf,
+    IronCurtain,
+    
+    // Third Armada
+    Fuckaround,
+    Hazard,
+    
+    // Fourth Armada
+    Basterd,
+    Sentinel
+}
+
+public enum ArmadaEnum
+{
+    FightingFirst,
+    RedDaggers,
+    Ravagers,
+    Spartans
+}
+
+public record AddScoreCommand(string ImageUrl, string PlayerName, ArmadaEnum PlayerArmada, FleetEnum PlayerFleet);
